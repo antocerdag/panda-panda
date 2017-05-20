1938bba43f7c458956b3c7d6b3024b7250e0b3db
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-19 23:38:34 -0400

    addeventlistener no funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
-document.getElementById("a1").addEventListener("onclick", eliminar());
-
-function eliminar(){
+document.getElementById("a1").addEventListener("onclick", function(){
 	var a1 = document.getElementById("a1");
-	var padre = a1.parentNode;
-	padre.removeChild(a1);
+	a1.parentNode.removeChild(a1);
+});
 
-}
+document.getElementById("a2").addEventListener("onclick", function(){
+	var a2 = document.getElementById("a2");
+	a2.parentNode.removeChild(a1);
+});
+
+document.getElementById("a3").addEventListener("onclick", function(){
+	var a3 = document.getElementById("a3");
+	a3.parentNode.removeChild(a3);
+});
+
+document.getElementById("a4").addEventListener("onclick", function(){
+	var a4 = document.getElementById("a4");
+	a4.parentNode.removeChild(a4);
+});
+
