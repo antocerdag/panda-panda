88cf6c84f5e5e1ab626e830dac2d1bf106a14276
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-19 21:02:54 -0400

    estructura html y css lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-main.js
+document.getElementById("a1").addEventListener("onclick", eliminar());
+
+function eliminar(){
+	var a1 = document.getElementById("a1");
+	var padre = a1.parentNode;
+	padre.removeChild(a1);
+
+}
